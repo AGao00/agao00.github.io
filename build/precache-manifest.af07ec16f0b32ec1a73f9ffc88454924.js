daf04c39b86b4df9f82d938777134a95cdada48b
Alyssa Gao <alyssa.gao@databricks.com>
2023-03-27 12:41:32 -0700

    add static assets

---
+++ build/precache-manifest.af07ec16f0b32ec1a73f9ffc88454924.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedbd451c5b127278fafe0b9a28110ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354ff92eac69431f2219",
+    "url": "/static/css/2.fb64e31e.chunk.css"
+  },
+  {
+    "revision": "c8ce6f9c6de6d3c64e04",
+    "url": "/static/css/main.e87b9eab.chunk.css"
+  },
+  {
+    "revision": "354ff92eac69431f2219",
+    "url": "/static/js/2.9a4dd271.chunk.js"
+  },
+  {
+    "revision": "c8ce6f9c6de6d3c64e04",
+    "url": "/static/js/main.6eff7295.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "041d46cdcb86bb4e399c53548a493b76",
+    "url": "/static/media/chevron-down.041d46cd.svg"
+  },
+  {
+    "revision": "56ac014955d8d6c576ff987534452590",
+    "url": "/static/media/dblogo.56ac0149.jpg"
+  },
+  {
+    "revision": "6df5d2c627bd4588f211d2cac956f6f3",
+    "url": "/static/media/speech-bubble-plus.6df5d2c6.svg"
+  }
+]);
